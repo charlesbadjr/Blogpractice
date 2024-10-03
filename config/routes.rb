0f9418fc2c0f799128ecb6c0c2b33d0c7b1d97c2
Charlie Jergins <39018811+charlesbadjr@users.noreply.github.com>
2024-10-03 06:42:40 -0600

    typo update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
-  get "/blog_posts/:id", to "blog_posts#show"
+  get "/blog_posts/:id", to: "blog_posts#show"
  # post 
  # put 
  # patch
